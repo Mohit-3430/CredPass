82139b6ce5b51445f4ccca59ccc8f2f67ae159b3
Mohit Suryadevara <75293052+Mohit-3430@users.noreply.github.com>
2022-03-05 21:42:00 +0530

    perf(navbar): removed req to server to fetch user name
    Fetching user name from local storage rather than a api call to the backend every time the component
    is rendered!!

--- client/src/components/VaultComponents/VaultNavbar.jsx
+++ client/src/components/VaultComponents/VaultNavbar.jsx
@@ -1,31 +1,15 @@
 import "../../styles/VaultNavbar.css"
 import {Link} from 'react-router-dom'
-import { useState, useEffect } from "react"
+import { useState} from "react"
 import { FaBars, FaTimes, FaUser, FaSignOutAlt, FaCaretDown, FaCaretUp} from "react-icons/fa";
 import { IoSettings } from "react-icons/io5";
-import axios from "axios"
 
-const VaultNavbar = ({uname}) => {
+const VaultNavbar = () => {
 
     const [showNav, setShowNav] = useState("hide")
     const [bars, setBars] = useState(true)
     const [extraMenu, setExtraMenu] = useState(false)
     const [caret, setCaret] = useState("down")
-    const [superUser, setSuperUser] = useState("")
-
-    useEffect(() => {
-        
-        const fetchSuperUserName = ()=>{
-            axios.get(`http://localhost:5000/api/vault-data/`, {
-            headers : {
-                "Authorization" : localStorage.getItem("token")
-            }
-            }).then(res =>{
-                    setSuperUser(res.data.user)
-            }).catch(err =>console.log(`${err}`))
-        }
-        fetchSuperUserName()
-    },[])
 
     const toggleNav = ()=>{
         if(showNav==="hide"){
@@ -64,7 +48,7 @@ const VaultNavbar = ({uname}) => {
                         {extraMenu && 
                         <ul>
                             <div className="toggle--extra__menu">
-                                <li className="user__name"><Link to='#'>@ {superUser}</Link></li>
+                                <li className="user__name"><Link to='#'>@ {localStorage.getItem('user')}</Link></li>
                                 <li><Link to='#'><FaUser/> Profile</Link></li>
                                 <li><Link to='/settings'><IoSettings /> Settings</Link></li>
                                 <li><Link to='/logout'><FaSignOutAlt /> Logout</Link></li>
