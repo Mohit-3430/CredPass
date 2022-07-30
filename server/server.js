25b18eeceed2458d0efd75ccbbea67aa1eab2c8a
Mohit Suryadevara <75293052+Mohit-3430@users.noreply.github.com>
2022-07-30 21:39:14 +0530

    refactor(deployment): ready for deployment

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ dotenv.config();
 
 const app = express();
 
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 app.use(express.json())
 app.use(cookieParser())
@@ -32,6 +32,14 @@ app.use('/api/user', UserAuthRoutes)
 
 // ====Vault Routes ====
 app.use('/api', authentication, toptCheck, VaultRoutes)
+
+import path from "path"
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static("client/build"))
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 app.listen(port, () => {
   console.log(`Runnning on port ${port}`);
