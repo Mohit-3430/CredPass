491a2bb29faaa030e0974682053262c99c34c7bd
Mohit Suryadevara <75293052+Mohit-3430@users.noreply.github.com>
2022-08-02 18:02:02 +0530

    refactor(rename): heroku fs is case sesitive

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose"
 import cookieParser from "cookie-parser";
-import { authentication, toptCheck } from "./src/middlewares/Authentication.js"
+import { authentication, toptCheck } from "./src/middlewares/authentication.js"
 import dotenv from "dotenv"
 dotenv.config()
 
