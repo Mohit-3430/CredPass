88d7d3a4f6458f4c0d31fc68ef96569689c331ce
Mohit Suryadevara <75293052+Mohit-3430@users.noreply.github.com>
2023-03-04 17:45:54 +0530

    fix(cors): fixed-cors issue

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json())
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: process.env.CLIENT_URL }))
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true, optionsSuccessStatus: 200 }))
 
 mongoose.set("strictQuery", false);
 mongoose.connect(process.env.DB_URI, { useNewUrlParser: true })
