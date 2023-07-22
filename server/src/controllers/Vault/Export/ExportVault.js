4dd4e698a0784fc0f2f2291398af71e274643693
Mohit Suryadevara <75293052+Mohit-3430@users.noreply.github.com>
2023-07-22 20:24:46 +0530

    fix(mongoose_find_callback): Fixed MongooseError
    MongooseError: Model.findOne() no longer accepts a callback at Function

--- server/src/controllers/Vault/Export/ExportVault.js
+++ server/src/controllers/Vault/Export/ExportVault.js
@@ -1,24 +1,21 @@
 import { Site } from "../../../Models/site.js";
 import { decrypt } from "../../../configs/EncryptionHandler.js"
 
-
 // GET /api/export-vault-data
 export const exportData = async (req, res) => {
     try {
-        Site.find({ user: req.user.sub }, 'siteUrl uname password', function (err, doc) {
-            if (err) {
-                console.log(err);
-                res.status(404).json({ success: false, msg: "error occured!" });
-            }
-            else {
-                for (let i = 0; i < doc.length; i++) {
-                    doc[i].password = decrypt(doc[i].password);
-                }
-                const finalData = JSON.stringify(doc, null, 2);
-                res.status(200).send(finalData);
-            }
+        const data = await Site.find({ user: req.user.sub })
+        const extractedData = data.map(obj => {
+            return {
+                site_name: obj.siteUrl,
+                user_name: obj.uname,
+                password: decrypt(obj.password),
+            };
         })
+
+        const finalData = JSON.stringify(extractedData, null, 2);
+        res.status(200).send(finalData);
     } catch (err) {
-        console.log(err);
+        res.status(404).json({ "success": false, "message": "An Error occurred!" })
     }
 }
