0acd6407d0908ceffa21c8e257b76719e482c673
Mohit Suryadevara <75293052+Mohit-3430@users.noreply.github.com>
2023-07-22 17:42:48 +0530

    feat(improv connection_status): Added Connection sucessed procedure

--- client/ConnectionStatus.jsx
+++ client/ConnectionStatus.jsx
@@ -5,16 +5,22 @@ import axios from "axios";
 
 const ConnectionStatus = () => {
   const [status, setStatus] = useState();
+  const [showComponent, setShowComponent] = useState(true);
 
   useEffect(() => {
     let isMounted = true;
     const getStatus = async () => {
       try {
         const resp = await axios.get(import.meta.env.VITE_SERVER_URL);
-        console.log(resp.status);
-        if (isMounted) setStatus(resp.status);
+        if (isMounted) {
+          setStatus(resp.status);
+          setShowComponent(true);
+          setTimeout(() => {
+            setShowComponent(false); // Hide the custom component after 5 seconds (adjust as needed).
+          }, 1500);
+        }
       } catch (err) {
-        setTimeout(getStatus, 200);
+        setTimeout(getStatus, 1000);
       }
     };
     getStatus();
@@ -27,9 +33,15 @@ const ConnectionStatus = () => {
   return (
     <>
       {status !== 200 && (
-        <div className="container">
+        <div className="container" id="fail">
           <FaInfo />
           Connecting to Server ...
+        </div>
+      )}
+      {showComponent && status === 200 && (
+        <div className="container" id="success">
+          <FaCheckCircle />
+          Connected to our Servers
         </div>
       )}
     </>
